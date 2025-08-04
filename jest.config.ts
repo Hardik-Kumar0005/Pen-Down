d1feeb9ebaaceff9fb5281107e925183593dcb52
Hardik-Kumar0005 <hardikkumarpro0005@gmail.com>
2025-08-05 00:23:45 +0530

    Fix gsap threeJS errors in testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,4 @@
+// jest.config.ts
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
 
@@ -9,6 +10,11 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  // Add this block to fix the error
+  transformIgnorePatterns: [
+    '/node_modules/(?!(gsap|@gsap/react|@react-three/drei|three)/)',
+  ],
 };
 
 export default createJestConfig(config);
